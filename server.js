24aebef612fadd0060a1874a25cce354ad730d8b
Khensani-Lebese <Khensanilebese0@gmail.com>
2025-01-21 07:43:31 +0200

    Added put and delete endpoints

--- server.js
+++ server.js
@@ -1,31 +1,47 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
-  if (url === '/' && method === 'GET') {
+  if (url === "/" && method === "GET") {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('My first node server.');
-  } else if (url === '/data' && method === 'POST') {
-    let body = '';
+    res.setHeader("Content-Type", "text/plain");
+    res.end("My first node server.");
+  } else if (url === "/" && method === "POST") {
+    let body = "";
 
-    req.on('data', chunk => {
+    req.on("data", (chunk) => {
       body += chunk.toString();
     });
 
-    req.on('end', () => {
+    req.on("end", () => {
       res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader("Content-Type", "application/json");
       res.end(`Received data: ${body}`);
     });
+  } else if (url === "/" && method === "PUT") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(`Updated data: ${body}`);
+    });
+  } else if (url === "/" && method === "DELETE") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Resource deleted successfully.");
   } else {
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Route not found');
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Route not found");
   }
 });
 
 server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+  console.log("Server running at http://localhost:3000/");
 });
